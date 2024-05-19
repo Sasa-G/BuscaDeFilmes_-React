f704fc597c5dfebcdd113b22127a2ec40018e09c
Samanta Gomes De Morais Santos <samantagdms@gmail.com>
2024-05-18 22:44:19 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BuscaDeFilmes from "./components/BuscaDeFilmes";
+
+function App(){
+  return(
+    <div className="App">
+      <BuscaDeFilmes/>
+    </div>
+  )
+}
+
+export default App;
